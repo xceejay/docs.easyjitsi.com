57add23d29a3183ce3bfab642ac325eda22e5ffc
Dominic Damoah MacBookPro <dominic@tsinvestmentgroup.com>
2020-04-11 15:43:45 +0000

    Commit from the Laptop. Quick Commit Format

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Easy Jitsi Docs', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
