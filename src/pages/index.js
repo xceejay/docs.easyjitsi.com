6c3d6f1762efd604282262872e4d60a600b45796
xceejay <joelkofiamoako@gmail.com>
2020-04-16 12:41:34 +0000

    removed old one and added new one

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import { Redirect } from "react-router-dom";
 
+<meta
+  name="google-site-verification"
+  content="8jJyJRNrGWnaAJIj3QADhjGestgjM4SzDSvdovQ986U"
+/>;
 
 function Home() {
   const context = useDocusaurusContext();
