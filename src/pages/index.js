29b1564034ac428be37e37dc920df4243ee22c10
xceejay <joelkofiamoako@gmail.com>
2020-04-21 18:13:07 +0000

    added meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,11 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import { Redirect } from "react-router-dom";
 
+<meta property="og:description" content="The Only Documentation You Need To Deploy Jitsi" />
+<meta name="description" content="The Only Documentation You Need To Deploy Jitsi" />
+
+
+
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
