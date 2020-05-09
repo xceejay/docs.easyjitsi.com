7078e6b631d27a890adb6f8b208cddc0871c5fa3
xceejay <joelkofiamoako@gmail.com>
2020-05-09 12:11:40 +0000

    yes

---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,99 @@
+export default {
+  "plugins": [
+    [
+      "@docusaurus/plugin-sitemap",
+      {
+        "cacheTime": 600000,
+        "changefreq": "weekly",
+        "priority": 0.5
+      }
+    ]
+  ],
+  "themes": [],
+  "customFields": {},
+  "themeConfig": {
+    "disableDarkMode": false,
+    "navbar": {
+      "title": "Easy Jitsi",
+      "logo": {
+        "alt": "Easy Jitsi",
+        "src": "img/logo.svg"
+      },
+      "links": [
+        {
+          "href": "https://easyjitsi.com",
+          "label": "Homepage",
+          "position": "right"
+        },
+        {
+          "href": "https://github.com/easyjitsi/docs.easyjitsi.com",
+          "label": "GitHub",
+          "position": "right"
+        },
+        {
+          "href": "https://github.com/easyjitsi/docs.easyjitsi.com/blob/master/LICENSE",
+          "label": "License",
+          "position": "right"
+        }
+      ]
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Home Page",
+              "to": "https://easyjitsi.com"
+            },
+            {
+              "label": "Help",
+              "to": "docs/help"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Telegram",
+              "href": "https://t.me/easyjitsisuppport"
+            }
+          ]
+        },
+        {
+          "title": "License",
+          "items": [
+            {
+              "label": "Apache 2.0",
+              "href": "https://github.com/easyjitsi/docs.easyjitsi.com/blob/master/LICENSE"
+            }
+          ]
+        }
+      ],
+      "copyright": "Copyright © 2020  Easy Jitsi"
+    }
+  },
+  "title": "Easy Jitsi Documentation",
+  "tagline": "The Only Documentation you'll Ever Need to Get Started with Jitsi",
+  "url": "https://docs.easyjitsi.com",
+  "baseUrl": "/",
+  "favicon": "img/favicon.ico",
+  "organizationName": "easyjitsi",
+  "projectName": "docs.easyjitsi.com",
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "/home/joel/Desktop/programming/ddc/easyjitsi/docs.easyjitsi.com/sidebars.js",
+          "editUrl": "https://github.com/easyjitsi/docs.easyjitsi.com"
+        },
+        "theme": {
+          "customCss": "/home/joel/Desktop/programming/ddc/easyjitsi/docs.easyjitsi.com/src/css/custom.css"
+        }
+      }
+    ]
+  ]
+};
---
+++ .docusaurus/routes.js
@@ -0,0 +1,204 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  
+{
+  path: '/',
+  component: ComponentCreator('/'),
+  exact: true,
+  
+},
+{
+  path: '/blog',
+  component: ComponentCreator('/blog'),
+  exact: true,
+  
+},
+{
+  path: '/blog/hello-world',
+  component: ComponentCreator('/blog/hello-world'),
+  exact: true,
+  
+},
+{
+  path: '/blog/hola',
+  component: ComponentCreator('/blog/hola'),
+  exact: true,
+  
+},
+{
+  path: '/blog/tags',
+  component: ComponentCreator('/blog/tags'),
+  exact: true,
+  
+},
+{
+  path: '/blog/tags/docusaurus',
+  component: ComponentCreator('/blog/tags/docusaurus'),
+  exact: true,
+  
+},
+{
+  path: '/blog/tags/facebook',
+  component: ComponentCreator('/blog/tags/facebook'),
+  exact: true,
+  
+},
+{
+  path: '/blog/tags/hello',
+  component: ComponentCreator('/blog/tags/hello'),
+  exact: true,
+  
+},
+{
+  path: '/blog/tags/hola',
+  component: ComponentCreator('/blog/tags/hola'),
+  exact: true,
+  
+},
+{
+  path: '/blog/welcome',
+  component: ComponentCreator('/blog/welcome'),
+  exact: true,
+  
+},
+{
+  path: '/docs/:route',
+  component: ComponentCreator('/docs/:route'),
+  
+  routes: [
+{
+  path: '/docs/architecture',
+  component: ComponentCreator('/docs/architecture'),
+  exact: true,
+  
+},
+{
+  path: '/docs/benchmarks',
+  component: ComponentCreator('/docs/benchmarks'),
+  exact: true,
+  
+},
+{
+  path: '/docs/config',
+  component: ComponentCreator('/docs/config'),
+  exact: true,
+  
+},
+{
+  path: '/docs/docker',
+  component: ComponentCreator('/docs/docker'),
+  exact: true,
+  
+},
+{
+  path: '/docs/extensions',
+  component: ComponentCreator('/docs/extensions'),
+  exact: true,
+  
+},
+{
+  path: '/docs/gettingstarted',
+  component: ComponentCreator('/docs/gettingstarted'),
+  exact: true,
+  
+},
+{
+  path: '/docs/gettingstartedjibri',
+  component: ComponentCreator('/docs/gettingstartedjibri'),
+  exact: true,
+  
+},
+{
+  path: '/docs/help',
+  component: ComponentCreator('/docs/help'),
+  exact: true,
+  
+},
+{
+  path: '/docs/iframeapi',
+  component: ComponentCreator('/docs/iframeapi'),
+  exact: true,
+  
+},
+{
+  path: '/docs/installation',
+  component: ComponentCreator('/docs/installation'),
+  exact: true,
+  
+},
+{
+  path: '/docs/installingdep',
+  component: ComponentCreator('/docs/installingdep'),
+  exact: true,
+  
+},
+{
+  path: '/docs/intro',
+  component: ComponentCreator('/docs/intro'),
+  exact: true,
+  
+},
+{
+  path: '/docs/introtojitsi',
+  component: ComponentCreator('/docs/introtojitsi'),
+  exact: true,
+  
+},
+{
+  path: '/docs/jibri',
+  component: ComponentCreator('/docs/jibri'),
+  exact: true,
+  
+},
+{
+  path: '/docs/jitsiui',
+  component: ComponentCreator('/docs/jitsiui'),
+  exact: true,
+  
+},
+{
+  path: '/docs/loadbalancing',
+  component: ComponentCreator('/docs/loadbalancing'),
+  exact: true,
+  
+},
+{
+  path: '/docs/quickstart',
+  component: ComponentCreator('/docs/quickstart'),
+  exact: true,
+  
+},
+{
+  path: '/docs/scaling',
+  component: ComponentCreator('/docs/scaling'),
+  exact: true,
+  
+},
+{
+  path: '/docs/securedomain',
+  component: ComponentCreator('/docs/securedomain'),
+  exact: true,
+  
+},
+{
+  path: '/docs/tips',
+  component: ComponentCreator('/docs/tips'),
+  exact: true,
+  
+},
+{
+  path: '/docs/welcome',
+  component: ComponentCreator('/docs/welcome'),
+  exact: true,
+  
+}],
+},
+  
+  {
+    path: '*',
+    component: ComponentCreator('*')
+  }
+];
